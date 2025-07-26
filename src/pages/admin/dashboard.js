d62953d55ed8108b8be766d97af6b2378c0d6e7d
tamawithai <tamawith.ai@gmail.com>
2025-07-26 21:46:30 +0700

    notifikasi

--- src/pages/admin/dashboard.js
+++ src/pages/admin/dashboard.js
@@ -43,9 +43,9 @@ export default function AdminDashboard() {
                   <Link href="/admin/articles" className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">
                     Artikel
                   </Link>
-                                  <Link href="/admin/moderation" className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">
-                                      Moderasi
-                                  </Link>
+                  <Link href="/admin/moderation" className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">
+                    Moderasi
+                  </Link>
                   <Link href="/admin/landing" className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">
                     Landing Page
                   </Link>
@@ -54,6 +54,26 @@ export default function AdminDashboard() {
                   </Link>
                 </div>
               </div>
+              {/* Notification Banner */}
+        {stats.find(stat => stat.name === 'Menunggu Moderasi' && parseInt(stat.value) > 0) && (
+          <div className="mt-4 bg-yellow-50 border-l-4 border-yellow-400 p-4">
+            <div className="flex">
+              <div className="flex-shrink-0">
+                <svg className="h-5 w-5 text-yellow-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                  <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
+                </svg>
+              </div>
+              <div className="ml-3">
+                <p className="text-sm text-yellow-700">
+                  Ada <span className="font-medium">{stats.find(stat => stat.name === 'Menunggu Moderasi').value}</span> artikel yang menunggu moderasi.{' '}
+                  <Link href="/admin/moderation" className="font-medium underline">
+                    Lihat daftar
+                  </Link>
+                </p>
+              </div>
+            </div>
+          </div>
+        )}
               <div className="flex items-center">
                 <div className="flex-shrink-0">
                   <button
@@ -85,27 +105,6 @@ export default function AdminDashboard() {
           </div>
         </nav>
 
-              {/* Notification Banner */}
-              {stats.find(stat => stat.name === 'Menunggu Moderasi' && parseInt(stat.value) > 0) && (
-                  <div className="mb-6 bg-yellow-50 border-l-4 border-yellow-400 p-4">
-                      <div className="flex">
-                          <div className="flex-shrink-0">
-                              <svg className="h-5 w-5 text-yellow-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                                  <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
-                              </svg>
-                          </div>
-                          <div className="ml-3">
-                              <p className="text-sm text-yellow-700">
-                                  Ada <span className="font-medium">{stats.find(stat => stat.name === 'Menunggu Moderasi').value}</span> artikel yang menunggu moderasi.{' '}
-                                  <Link href="/admin/moderation" className="font-medium underline">
-                                      Lihat daftar
-                                  </Link>
-                              </p>
-                          </div>
-                      </div>
-                  </div>
-              )}
-
         {/* Main Content */}
         <main className="py-6">
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -122,7 +121,7 @@ export default function AdminDashboard() {
                     <div className="flex items-center">
                       <div className="flex-shrink-0 bg-primary-100 rounded-md p-3">
                         <svg className="h-6 w-6 text-primary-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
+                          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 19v-6a2 2 0 012-2h2a2 2 0 012 2v6a2 2 0 01-2 2h-2a2 2 0 01-2-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10a2 2 0 01-2 2h-2a2 2 0 01-2-2zm0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
                         </svg>
                       </div>
                       <div className="ml-5 w-0 flex-1">
